a91e92dd0af5691721ad627f662516a52221b390
su10 <su10.hatena@gmail.com>
2014-03-19 01:49:10 +0900

    add socket.io configuration

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = require('http').createServer(function(req, res) {
   res.end(fs.readFileSync('index.html'));  
 }).listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(app);
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
 io.sockets.on('connection', function(socket) {
   socket.on('msg', function(data) {
     io.sockets.emit('msg', data);
